878aec64b13a5557140d15cfea9a64ce51a70ae6
ingdanni <ing.danni@gmail.com>
2018-09-03 12:55:08 -0600

    Updated app port.

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const schema = require('./schema/schema');
 
 const app = express();
 
+const port = process.env.port || 4000;
+
 app.use('/graphql', expressGraphQL({
   schema,
   graphiql: true
 }));
 
-app.listen(4000, () => {
+app.listen(port, () => {
   console.log('Listening');
 });
